fcabacb7c427ac34be99038c8f37f992a025f9e2
Jose A González <jagascripts@gmail.com>
2022-01-26 06:25:40 +0100

    Creadas todas las entidades, agregado gradle projecto lombok y mapstruct con anotation procesors, solo la clase cliente con @Data anotation, pendientes de revisión clases Cliente y Categoría

---
+++ src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -0,0 +1,30 @@
+package com.platzi.market.persistence.entity;
+
+import jdk.jfr.DataAmount;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "Clientes")
+public class Cliente {
+
+    @Id
+    private String id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private Integer celular;
+
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+}
