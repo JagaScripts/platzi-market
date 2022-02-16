1de5453339fee33aff460862c13ce0c371b34f82
Jose A González <jagascripts@gmail.com>
2022-02-16 08:16:56 +0100

    Finalizada la configuración de segurida JWT

--- src/main/java/com/platzi/market/domain/service/PlatziUserDetailsService.java
+++ src/main/java/com/platzi/market/domain/service/PlatziUserDetailsService.java
@@ -7,12 +7,27 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlatziUserDetailsService implements UserDetailsService {
 
+    private static List<User> users = new ArrayList();
+
+    public PlatziUserDetailsService(){
+        users.add(new User("jose", "{noop}platzi", new ArrayList<>()));
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("jose", "{noop}contraseña", new ArrayList<>());
+    public UserDetails loadUserByUsername(String username)throws UsernameNotFoundException {
+        //TODO ir a BD por ussuarios o sistemas AUTH0
+        Optional<User> user = users.stream().filter(u -> u.getUsername().equals(username)).findAny();
+
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("User not found by name: " + username);
+        }
+
+        return new User(user.get().getUsername(),user.get().getPassword(), user.get().getAuthorities());
     }
 }
